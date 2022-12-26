19f24b15ef22ac4e4ec0b17dc2af9cb0c1bb14c9
Truong An Nguyen <tanguyen21@myseneca.ca>
2022-12-26 08:20:46 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {Fragment} from "react";
+
+function App() {
+  return (
+    <Fragment>
+        <h1>Welcome to ProShop</h1>
+    </Fragment>
+  );
+}
+
+export default App;
