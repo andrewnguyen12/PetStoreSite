29e3799f464f57412f3994f3d2bb05905784314e
Truong An Nguyen <tanguyen21@myseneca.ca>
2022-12-26 16:26:19 -0500

    Header and Footer 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
 import {Fragment} from "react";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import {Container, Row, Col} from "react-bootstrap"
 
 function App() {
   return (
     <Fragment>
-        <h1>Welcome to ProShop</h1>
+        <Header/>
+        <main>
+            <Container>
+                <h1 className='text-center py-3'>Welcome to ProShop</h1>
+            </Container>
+        </main>
+        <Footer/>
     </Fragment>
   );
 }
